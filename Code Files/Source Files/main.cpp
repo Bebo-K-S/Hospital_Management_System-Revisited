ad649f3e9654108e89579b905a0554d850831e6c
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-08-06 17:49:15 +0300

    Added the latest files
    Since i was working on it without updating thought to upload my progress so far would help me in focusing on this project much more.

---
+++ Code Files/Source Files/main.cpp
@@ -0,0 +1,32 @@
+/*Include source files*/
+#include "login.cpp"
+
+int main()
+{
+    int* scene = new int(0);
+
+    Admin admin;
+
+    while (*scene != -1)
+    {
+        // A switch case that will switch between the different scenes
+        // present in the program by passing the pointer int to those
+        // different scene functions where it'll change accordingly
+        switch (*scene)
+        {
+            case 0: // Login page
+                runLoginScreen(scene, &admin);
+                break;
+            /*
+              .
+              .
+              .
+              .
+            */
+        }
+    }
+
+    cout << "Thank you & bye!!";
+
+    return 0;
+}
