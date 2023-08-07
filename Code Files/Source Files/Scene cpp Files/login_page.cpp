b5404d504908712cf00685ac8cae6d5a30bfc360
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-08-08 02:23:06 +0300

    Placed the login scene file in it's respective folder

--- Code Files/Source Files/Scene cpp Files/login_page.cpp
+++ Code Files/Source Files/Scene cpp Files/login_page.cpp
@@ -6,27 +6,27 @@
 using namespace std;
 using namespace sf;
 
-inline void runLoginScreen(int* scene, Admin *admin)
+inline void runLoginScreen(int* scene, Admin *admin, RenderWindow *window)
 {
-    RenderWindow window(VideoMode(1600, 900), "Login Screen");
-    window.setVerticalSyncEnabled(true);
-
+    /* Loading the Inika font and if failed to do so close the program */
     Font font;
-    if (!font.loadFromFile("Assets/Font/Noto_Serif_Khitan_Small_Script/NotoSerifKhitanSmallScript-Regular.ttf"))
+    if (!font.loadFromFile("Assets/Font/Inika/Inika-Bold.ttf"))
     {
         cout << "Error loading font" << endl;
         *scene = -1;
         return;
     }
 
+    /* Loading the background and if failed to do so close the program */
     Texture backgroundTexture;
-    if (!backgroundTexture.loadFromFile("Assets/Backgrounds/HBM-Login_Page.jpg"))
+    if (!backgroundTexture.loadFromFile("Assets/Backgrounds/HMS-Login_Page.jpg"))
     {
         cout << "Error loading background image\n";
             *scene = -1;
             return;
     }
 
+    /* Beginning of declaring shapes and texts to use */
     sf::Color transparentColor(255, 255, 255, 0);
 
     RectangleShape background(Vector2f(1600, 900));
@@ -57,28 +57,30 @@ inline void runLoginScreen(int* scene, Admin *admin)
     string usernameInput = "";
     string passwordInput = "";
 
-    while (window.isOpen())
+    /* End of declaring */
+
+    while (window->isOpen())
     {
         Event event;
-        while (window.pollEvent(event))
+        while (window->pollEvent(event))
         {
             if (event.type == Event::Closed)
             {
                 *scene = -1;
-                window.close();
+                window->close();
             }
 
             if (event.type == Event::TextEntered)
             {
                 if (event.text.unicode == 8) // Check for the backspace character explicitly
                 {
-                    if (usernameInput.size() > 0 && usernameBox.getGlobalBounds().contains(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+                    if (usernameInput.size() > 0 && usernameBox.getGlobalBounds().contains(Mouse::getPosition(*window).x, Mouse::getPosition(*window).y))
                     {
                         usernameInput.erase(usernameInput.end() - 1); // Remove the last character from the input string
                         usernameInputText.setString(usernameInput); // Update the input text object with the new input string
                     }
 
-                    if (passwordInput.size() > 0 && passwordBox.getGlobalBounds().contains(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+                    if (passwordInput.size() > 0 && passwordBox.getGlobalBounds().contains(Mouse::getPosition(*window).x, Mouse::getPosition(*window).y))
                     {
                         passwordInput.erase(passwordInput.end() - 1); // Remove the last character from the input string
                         passwordInputText.setString(string(passwordInput.size(), '*')); // Update the input text object with a string of asterisks
@@ -86,13 +88,13 @@ inline void runLoginScreen(int* scene, Admin *admin)
                 }
                 else if (event.text.unicode >= 32 && event.text.unicode < 127) // Check for printable ASCII characters
                 {
-                    if (usernameBox.getGlobalBounds().contains(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+                    if (usernameBox.getGlobalBounds().contains(Mouse::getPosition(*window).x, Mouse::getPosition(*window).y))
                     {
                         usernameInput += static_cast<char>(event.text.unicode);
                         usernameInputText.setString(usernameInput); // Update the input text object with the new input string
                     }
 
-                    if (passwordBox.getGlobalBounds().contains(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+                    if (passwordBox.getGlobalBounds().contains(Mouse::getPosition(*window).x, Mouse::getPosition(*window).y))
                     {
                         passwordInput += static_cast<char>(event.text.unicode);
                         passwordInputText.setString(string(passwordInput.size(), '*')); // Update the input text object with a string of asterisks
@@ -102,12 +104,12 @@ inline void runLoginScreen(int* scene, Admin *admin)
 
             if (event.type == Event::KeyPressed || event.type == Event::MouseButtonPressed)
             {
-                if (event.key.code == Keyboard::Return || loginText.getGlobalBounds().contains(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+                if (event.key.code == Keyboard::Return || loginText.getGlobalBounds().contains(Mouse::getPosition(*window).x, Mouse::getPosition(*window).y))
                 {
-                    if (usernameInput == "ADMIN" && passwordInput == "123") // 5218550
+                    if (usernameInput == "ADMIN" && passwordInput == "123") // PASSWORD: 5218550
                     {
                         cout << "Login successful" << endl;
-                        *scene = -1; // Admin's Page
+                        *scene = 1; // Admin's Page
                         return;
                     }
                     else if (admin->findDoctorName(usernameInput) && admin->findDoctorID(passwordInput))
@@ -137,20 +139,20 @@ inline void runLoginScreen(int* scene, Admin *admin)
         }
 
         loginText.setFillColor(Color(0xF6, 0xF1, 0xF1));
-        if (loginText.getGlobalBounds().contains(Mouse::getPosition(window).x, Mouse::getPosition(window).y))
+        if (loginText.getGlobalBounds().contains(Mouse::getPosition(*window).x, Mouse::getPosition(*window).y))
         {
             loginText.setFillColor(Color(0xAF, 0xD3, 0xE2));
         }
 
-        window.clear(Color::White);
-        window.draw(background);
-        window.draw(usernameBox);
-        window.draw(passwordBox);
+        window->clear(Color::White);
+        window->draw(background);
+        window->draw(usernameBox);
+        window->draw(passwordBox);
 
-        window.draw(usernameInputText);
-        window.draw(passwordInputText);
+        window->draw(usernameInputText);
+        window->draw(passwordInputText);
 
-        window.draw(loginText);
-        window.display();
+        window->draw(loginText);
+        window->display();
     }
 }
